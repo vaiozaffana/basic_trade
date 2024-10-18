da86ae34502cf6fc47bf5bf2931b5f3f53114112
vaiozaffana <vaiozaffana>
2024-10-18 10:03:11 +0700

    chore: adjusment for deployment

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"BasicTrade/database"
 	"BasicTrade/routes"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -11,8 +13,30 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		panic("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	jwtSecretKey := os.Getenv("JWT_SECRET_KEY")
+
+	cloudinaryApiKey := os.Getenv("CLOUDINARY_API_KEY")
+	cloudinaryApiSecret := os.Getenv("CLOUDINARY_API_SECRET")
+	cloudinaryCloudName := os.Getenv("CLOUDINARY_CLOUD_NAME")
+
+	fmt.Println("Database Host: ", dbHost)
+	fmt.Println("Database User: ", dbUser)
+	fmt.Println("Database Password: ", dbPassword)
+	fmt.Println("Database Name: ", dbName)
+	fmt.Println("Database Port: ", dbPort)
+	fmt.Println("JWT Secret Key: ", jwtSecretKey)
+	fmt.Println("Cloudinary API Key: ", cloudinaryApiKey)
+	fmt.Println("Cloudinary API Secret: ", cloudinaryApiSecret)
+	fmt.Println("Cloudinary Cloud Name: ", cloudinaryCloudName)
 
 	database.InitDB()
 
